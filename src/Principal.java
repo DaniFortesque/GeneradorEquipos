6e6f3cf17d548844f548f79f58bb13db94baf77c
Dani M <daniel.ma36933@cesurformacion.com>
2021-01-27 12:50:33 +0100

    modificacion primer ejercicio

--- src/Principal.java
+++ src/Principal.java
@@ -2,18 +2,15 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		
 		String[] nombre = {"Rafa","Carmen","David","Alberto", "Guille","Alejandro", "Jorge", "Furgen", "Adrian", "Berta", "Marileni", "Marta", "Ana Cristina", "Marian", "Susana", "Dani"};
-		
 		String[] apellidos = {"Marquez", "Molina", "Martinez", "Sánchez", "Puerta", "Pérez","López", "Carrillo", "Tunéz", "Rodriguez", "Lozano", "Moreno", "Marín", "Iglesias", "Box", "Gutierrez", "Torres", "Fuentes", "Cross", "Mendizabal", "Tapias", "Carvajal"};
-		
 		String[] posicion = {"Portero", "Defensa", "Centrocampista","Delantero"};
 		
-		System.out.println("¿Cuantos jugadores quieres que tenga tu equipo?");
+		/*System.out.println("¿Cuantos jugadores quieres que tenga tu equipo?");
 		int numeroJugadores = sc.nextInt();
 		
-		Jugador[] equipo = new Jugador[numeroJugadores];
+		
 		
 		generarEquipos(equipo, nombre, apellidos, posicion, numeroJugadores);
 		
@@ -21,10 +18,15 @@ public class Principal {
 		for(int i=0; i<numeroJugadores; i++) {
 			System.out.println(equipo[i].getNombre()+" "+equipo[i].getApellidos()+" "+equipo[i].getPosicion()+" "+equipo[i].getDorsal());
 			
-		}		
+		}*/		
 	}
 	
-	public static void generarEquipos(Jugador[] equipo, String[] nombre, String[] apellidos, String[] posicion, int numeroJugadores) {
+	public static void menu() {
+		Scanner sc = new Scanner(System.in);
+	}
+	
+	public static Jugador[] generarEquipos(String[] nombre, String[] apellidos, String[] posicion, int numeroJugadores) {
+		Jugador[] equipo = new Jugador[numeroJugadores];
 		for(int i=0; i<numeroJugadores; i++) {
 			Jugador jugador = new Jugador("","","",i+1);
 			jugador.setNombre(nombre[(int) (Math.random()*16)]);
@@ -32,8 +34,8 @@ public class Principal {
 			jugador.setPosicion(posicion[(int) (Math.random()*4)]);
 			jugador.setDorsal(i+1);
 			equipo[i]=jugador;
-			//equipo[i] = new Jugador(nombre[(int) (Math.random()*16)], apellidos[(int) (Math.random()*22)], posicion[(int) (Math.random()*4)], i+1); 
 		}
+		return equipo;
 	}
 
 //setNombre(nombre[(int) (Math.random()*16)]),setApellidos(apellidos[(int) (Math.random()*22)]), setPosicion(posicion[(int) (Math.random()*4)]
