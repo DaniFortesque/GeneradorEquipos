480af7066e184d0355a5be67af7ef57d04af7272
Dani M <daniel.ma36933@cesurformacion.com>
2021-01-28 12:26:17 +0100

    modificacion dani

--- src/Principal.java
+++ src/Principal.java
@@ -1,29 +1,8 @@
 import java.util.Scanner;
 public class Principal {
 
-<<<<<<< HEAD
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String[] nombre = {"Rafa","Carmen","David","Alberto", "Guille","Alejandro", "Jorge", "Furgen", "Adrian", "Berta", "Marileni", "Marta", "Ana Cristina", "Marian", "Susana", "Dani"};
-		String[] apellidos = {"Marquez", "Molina", "Martinez", "Sánchez", "Puerta", "Pérez","López", "Carrillo", "Tunéz", "Rodriguez", "Lozano", "Moreno", "Marín", "Iglesias", "Box", "Gutierrez", "Torres", "Fuentes", "Cross", "Mendizabal", "Tapias", "Carvajal"};
-		String[] posicion = {"Portero", "Defensa", "Centrocampista","Delantero"};
-		
-		System.out.println("¿Cuantos jugadores quieres que tenga tu equipo?");
-		int numeroJugadores = sc.nextInt();
-		Jugador[] equipo = new Jugador[numeroJugadores];
-		
-		
-		generarEquipos(equipo, nombre, apellidos, posicion, numeroJugadores);
-		
-		System.out.println("Tu equipo es: ");
-		for(int i=0; i<numeroJugadores; i++) {
-			System.out.println(equipo[i].getNombre()+" "+equipo[i].getApellidos()+" "+equipo[i].getPosicion()+" "+equipo[i].getDorsal());
-			
-		}	
-=======
 	public static void main(String[] args) {		
 		menu();
->>>>>>> e26789eafdf141f81326106be6864738f5d62fd7
 	}
 	
 	public static void menu(){
@@ -41,10 +20,6 @@ public class Principal {
 		}
 	}
 	
-<<<<<<< HEAD
-	public static Jugador[] generarEquipos(Jugador[] equipo, String[] nombre, String[] apellidos, String[] posicion, int numeroJugadores) {
-		
-=======
 	
 	public static Jugador[] generarEquipos() {
 		Scanner sc = new Scanner(System.in);
@@ -52,11 +27,10 @@ public class Principal {
 		String[] apellidos = {"Marquez", "Molina", "Martinez", "Sanchez", "Puerta","Yepes", "Perez","Lopez", "Carrillo", "Tunez", "Rodriguez", "Lozano", "Moreno", "Marin", "Iglesias", "Box", "Gutierrez", "Torres", "Fuentes", "Cross", "Mendizabal", "Tapias", "Carvajal"};
 		String[] posicion = {"Portero", "Defensa", "Centrocampista","Delantero"};
 		
-		System.out.println("�Cuantos jugadores quieres que tenga tu equipo?");
+		System.out.println("Cuantos jugadores quieres que tenga tu equipo?");
 		int numeroJugadores = sc.nextInt();
 		
 		Jugador[] equipo = new Jugador[numeroJugadores];
->>>>>>> e26789eafdf141f81326106be6864738f5d62fd7
 		for(int i=0; i<numeroJugadores; i++) {
 			Jugador jugador = new Jugador("","","",i+1);
 			jugador.setNombre(nombre[(int) (Math.random()*16)]);
@@ -67,8 +41,4 @@ public class Principal {
 		}
 		return equipo;
 	}
-
-//setNombre(nombre[(int) (Math.random()*16)]),setApellidos(apellidos[(int) (Math.random()*22)]), setPosicion(posicion[(int) (Math.random()*4)]
-	
-
 }
