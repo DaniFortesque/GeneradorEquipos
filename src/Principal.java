f71cbf458664c7438576393175d8756815c99a0f
Dani <martinezarrabal85@gmail.com>
2021-01-31 12:23:29 +0100

    he añadido cargarEquipo

--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,10 @@
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.Scanner;
+
 public class Principal {
 
 	public static Jugador[] equipo = {}; //Creamos una variable global ya que va a ser compartida por todo el programa
@@ -16,15 +19,21 @@ public class Principal {
 		while(!salir) {
 			System.out.println("Introduce una opcion:");
 			System.out.println("(1) Generar equipo");
-			System.out.println("(2) Imprimir Equipo");
-			System.out.println("(3) Salir");
+			System.out.println("(2) Cargar Equipo");
+			System.out.println("(3) imprimir jugador");
 			int opcion= sc.nextInt();
 			switch (opcion) {
 				case 1: //Genera el equipo, lo imprime en consola y lo guarda en un txt
 					equipo = generarEquipos();
 					imprimirEquipo(equipo);
 					guardarEquipo(equipo);
+					break;
 				case 2:
+					cargarEquipo();
+					break;
+				case 3:
+					System.out.println(equipo[3].getJugador());
+					break;
 					
 					
 			}
@@ -40,19 +49,19 @@ public class Principal {
 		System.out.println("¿Cuantos jugadores quieres que tenga tu equipo?");
 		int numeroJugadores = sc.nextInt();
 		
-		Jugador[] equipo = new Jugador[numeroJugadores];
+		equipo = new Jugador[numeroJugadores];
 		for(int i=0; i<numeroJugadores; i++) { //Este for va generando los jugadores y los añade en una posicion de la array
-			Jugador jugador = new Jugador("","","",i+1);
+			Jugador jugador = new Jugador("","","","");
 			jugador.setNombre(nombre[(int) (Math.random()*16)]);
 			jugador.setApellidos(apellidos[(int) (Math.random()*22)], apellidos[(int) (Math.random()*22)]);
 			jugador.setPosicion(probabilidadPosicion((int) (Math.random()*9)));
-			jugador.setDorsal(i+1);
+			jugador.setDorsal(""+(i+1));
 			equipo[i]=jugador;
 		}
 		return equipo;
 	}
 	
-	public static String probabilidadPosicion(int i){ //Establece probabilidades a cada posicion
+	public static String probabilidadPosicion(int i){ //Establece probabilidades de aparicion a cada posicion
 		if(i==0) { //10%
 			return "Portero";
 		}else if(i>0 && i<4) { //30%
@@ -71,8 +80,8 @@ public class Principal {
 		}
 	}
 	
-	public static void guardarEquipo(Jugador[] equipo) { //ESta funcion crea un txt y guarda el equipo en el
-		try {
+	public static void guardarEquipo(Jugador[] equipo) { 			//Esta funcion crea un txt y guarda el equipo en el
+		try {														//con un formato especifico para que pueda ser recuperado
 			String sCarpAct = System.getProperty("user.dir");
 			File carpeta = new File(sCarpAct);
 			String ruta = carpeta+"/src/MiEquipo.txt";
@@ -86,7 +95,7 @@ public class Principal {
 	        FileWriter fw = new FileWriter(file);
             BufferedWriter bw = new BufferedWriter(fw);
             for(int i=0; i<equipo.length; i++) {
-            	bw.write(equipo[i].getDorsal()+"/"+equipo[i].getNombre()+"/"+equipo[i].getApellidos()+"/"+equipo[i].getDorsal()+"\n");
+            	bw.write(equipo[i].getDorsal()+"/"+equipo[i].getNombre()+"/"+equipo[i].getApellidos()+"/"+equipo[i].getPosicion()+"/"+"\n");
             }
             bw.close();
             
@@ -94,6 +103,79 @@ public class Principal {
             e.printStackTrace();
         }
 	}
+	
+	public static void cargarEquipo() {  //Esta funcion abre el archivo donde se guarda el equipo, lo lee y separa sus atributos
+										 //para asignarlos a la clase jugador y darles una posicion en la array equipo
+	    FileReader fr = null;
+	    BufferedReader br = null;
+		String sCarpAct = System.getProperty("user.dir");
+		File carpeta = new File(sCarpAct);
+		String ruta = carpeta+"/src/MiEquipo.txt";
+		
+		try {
+	         // Apertura del fichero y creacion de BufferedReader para poder
+	         // hacer una lectura comoda (disponer del metodo readLine()).
+			 fr = new FileReader(ruta);
+			 br = new BufferedReader(fr);
+			 br.mark(100000); //ponemos una marca al principio para volver a ella despues
+
+	         // Lectura del fichero
+	         String linea;
+	         int i=0;
+	         while((linea=br.readLine())!=null) { //cuenta el numero de lineas/jugadores para crear la array
+	        	 i++;
+	         }
+	         
+	         equipo = new Jugador[i];
+	         int contador = 0; //cuenta el numero de '/' que va encontrando al leer la linea
+	         String cadena = "";
+	         br.reset(); //reseteamos la posicion de lectura a la marca inicial
+	         
+	         for(int e=0; e<i; e++) { //recorre cada posicion de la array
+	        	 Jugador jugador = new Jugador("","","",""); //creamos un jugador 'vacio'
+	        	 linea=br.readLine();
+	        	 
+	        	 for(int a=0; a<linea.length(); a++) { //recorre cada caracter de la linea leida y los separa en atributos
+	        		 if (linea.charAt(a)!='/') {
+	        			 cadena = cadena+linea.charAt(a); //almacena cada caracter en una cadena hasta que se encuentra con un '/'
+	        		 }else if(linea.charAt(a)=='/' && contador==0) {
+	        			 jugador.setDorsal(cadena);
+	        			 cadena="";
+	        			 contador++;
+	        		 }else if(linea.charAt(a)=='/' && contador==1) {
+	        			 jugador.setNombre(cadena);
+	        			 cadena="";
+	        			 contador++;
+	        		 }else if(linea.charAt(a)=='/' && contador==2) {
+	        			 jugador.setApellidos(cadena);
+	        			 cadena="";
+	        			 contador++;
+	        		 }else if(linea.charAt(a)=='/' && contador==3) {
+	        			 jugador.setPosicion(cadena); 
+	        			 cadena="";
+	        			 contador=0;
+	        		 }
+	        	 }
+	        	 equipo[e]=jugador; //se almacena el jugador creado en su posicion de la array
+	         }
+		}
+	      	 catch(Exception e){
+	         e.printStackTrace();
+	      	 }finally{
+	         // En el finally cerramos el fichero, para asegurarnos
+	         // que se cierra tanto si todo va bien como si salta 
+	         // una excepcion.
+	         try{                    
+	            if( null != fr ){   
+	               fr.close();     
+	            }                  
+	         }catch (Exception e2){ 
+	            e2.printStackTrace();
+	         }
+	    }
+	}
 }
 
 //https://decodigo.com/java-crear-archivos-de-texto --- crear archivo de texto y escribir
+//http://chuwiki.chuidiang.org/index.php?title=Lectura_y_Escritura_de_Ficheros_en_Java --- leer un archivo de texto
+//http://ayudaitver.blogspot.com/2014/07/uso-basico-del-bufferedreader.html --- uso de los metodos de BufferedReader
